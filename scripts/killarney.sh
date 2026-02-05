2be09218ec09ae488606b85d1be80b9f6366136d
Andreas Burger <andreas.burger@mail.utoronto.ca>
2026-02-05 18:24:27 -0500

    ==

--- scripts/killarney.sh
+++ scripts/killarney.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 #SBATCH -A aip-aspuru
-#SBATCH -D /project/aip-aspuru/aburger/gad-ff
+#SBATCH -D /scratch/aburger/gad-ff
 #SBATCH --time=71:00:00
 #SBATCH --gres=gpu:l40s:1 
 #SBATCH --mem=128GB
 #SBATCH --job-name=gad 
 # Jobs must write their output to your scratch or project directory (home is read-only on compute nodes).
-#SBATCH --output=/project/aip-aspuru/aburger/gad-ff/outslurm/slurm-%j.txt 
-#SBATCH --error=/project/aip-aspuru/aburger/gad-ff/outslurm/slurm-%j.txt
+#SBATCH --output=/scratch/aburger/gad-ff/outslurm/slurm-%j.txt 
+#SBATCH --error=/scratch/aburger/gad-ff/outslurm/slurm-%j.txt
 
 # activate venv
-source ${PYTHONBIN}/activate
+source .venv/bin/activate
+
+which python
 
 # get environment variables
-source .env
+# source .env
 
 #module load cuda/12.6
 #module load gcc/12.3
@@ -25,6 +27,7 @@ echo `date`: Job $SLURM_JOB_ID is allocated resources.
 echo "Inside slurm_launcher.slrm ($0). received arguments: $@"
 
 # hand over all arguments to the script
-echo "Submitting ${HOMEROOT}/$@"
+pwd
+echo "Submitting $@"
 
-${PYTHONBIN}/python ${HOMEROOT}/"$@"
+srun uv run "$@"
